664f1d088d028c03e06fd54daa17d6e90c67b62a
Brian <brian.stith0331@gmail.com>
2024-04-12 19:41:37 -0400

    removed commented calulators (Need to fix the calculations). Added java.utils for mapping to maybe try

--- src/main/java/com/ps/main.java
+++ src/main/java/com/ps/main.java
@@ -1,8 +1,8 @@
 package com.ps;
 
 import java.util.Scanner;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
 public class main {
 
@@ -10,7 +10,13 @@ public class main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please select from the flowing calculators:\n" +
+        /*
+        Attempt at mapping the inputs instead of switch statements.
+        Map<Integer> calculatorMap = new
+
+         */
+
+        System.out.println("Please select from the following calculators:\n" +
                 "1. Mortgage Calculator\n" +
                 "2. Future Value Calculator\n" +
                 "3. Present Value of Annuity\n");
@@ -33,84 +39,6 @@ public class main {
         }
 
         scanner.close();
-
-
-//For later
-//Calculator 1
-/*
-        // User will be importing their information
-
-        public static void mortgageCalculator(Scanner scanner){
-        System.out.print("What is the principal value of the mortgage: ");
-        double principalMortgage = scanner.nextDouble();
-        System.out.print("What is your annual interest rate: ");
-        float annualInterestRate = scanner.nextFloat();
-        System.out.print("What is the terms of the loan: ");
-        int loanLengthYears = scanner.nextInt();
-
-        //Calculation of the given information
-        double monthlyInterestRate = (annualInterestRate / 100 / 12);
-        int numberOfPayments = (loanLengthYears * 12);
-        double monthlyPayment = (principalMortgage * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
-        double totalPayment = (monthlyPayment*numberOfPayments);
-        double totalInterest = (totalPayment - principalMortgage);
-
-        //Display Results
-
-        System.out.printf("Monthly payment: $%.2f ", monthlyPayment);
-        System.out.print(String.format("Total interest paid: $%.2f", totalInterest));
-        }
-
-*/
-
-//Calculator 2
-/*
-
-        //User inputs initial CD values
-
-        public static void futureValueCalculator(Scanner scanner){
-        System.out.print("What was your initial Deposit Amount: ");
-        int initialDeposit = scanner.nextInt();
-        System.out.print("What is your interest rate: ");
-        float futureInterestRate = scanner.nextFloat();
-        System.out.print("When will your CD mature: ");
-        int cdMaturation = scanner.nextInt();
-
-        //Calculations of the Given information
-
-        double futureValue = initialDeposit * Math.pow(1 + (futureInterestRate / 100), cdMaturation );
-        double totalInterestEarned = futureValue - initialDeposit;
-
-        //Display the results of the CD
-
-        System.out.printf("Your future value is: $%.2f ", futureValue);
-        System.out.print(String.format("The total amount of interest earned is: $%.2f", totalInterestEarned));
-        }
-
-*/
-
-//Calculator 3
-/*
-        //User inputs their Annuity responses
-        public static void annuityPresentValueCalculator(Scanner scanner){
-        System.out.print("Enter your monthly payout amount: ");
-        double monthlyPayout = scanner.nextDouble();
-        System.out.print("Enter your expected interest rate: ");
-        float expectedInterestRate = scanner.nextFloat();
-        System.out.print("Enter your number of years: ");
-        int yearsPayout = scanner.nextInt();
-
-        //Calculations of the inputted data
-
-       double monthlyInterestAnnuity = (expectedInterestRate / 100 / 12);
-       int paymentNumbers = (yearsPayout * 12);
-       double presentValue = monthlyPayout * (1 - Math.pow(1 + monthlyInterestAnnuity, -yearsPayout )) / monthlyInterestAnnuity;
-
-       //Display the results of the annuity
-
-        System.out.printf("Present value of the annuity is: %.2f", presentValue);
-        }
-*/
 
     }
 
@@ -139,7 +67,6 @@ public class main {
 //Calculator 2
 
         //User inputs initial CD values
-
         public static void futureValueCalculator(Scanner scanner){
         System.out.print("What was your initial Deposit Amount: ");
         int initialDeposit = scanner.nextInt();
@@ -149,12 +76,10 @@ public class main {
         int cdMaturation = scanner.nextInt();
 
         //Calculations of the Given information
-
         double futureValue = initialDeposit * Math.pow(1 + (futureInterestRate / 100), cdMaturation );
         double totalInterestEarned = futureValue - initialDeposit;
 
         //Display the results of the CD
-
         System.out.printf("Your future value is: $%.2f\n", futureValue);
         System.out.print(String.format("The total amount of interest earned is: $%.2f", totalInterestEarned));
         }
@@ -172,13 +97,11 @@ public class main {
         int yearsPayout = scanner.nextInt();
 
         //Calculations of the inputted data
-
        double monthlyInterestAnnuity = (expectedInterestRate / 100 / 12);
        int paymentNumbers = (yearsPayout * 12);
        double presentValue = monthlyPayout * (1 - Math.pow(1 + monthlyInterestAnnuity, -yearsPayout )) / monthlyInterestAnnuity;
 
        //Display the results of the annuity
-
         System.out.printf("Present value of the annuity is: %.2f", presentValue);
         }
 }
